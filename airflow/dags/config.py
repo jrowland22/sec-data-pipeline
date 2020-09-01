14a959838f5e5ab7c3a1b3f1d90d0be5aec01768
jrowland22 <42189132+jrowland22@users.noreply.github.com>
2020-09-01 15:19:59 -0400

    Create config.py

---
+++ airflow/dags/config.py
@@ -0,0 +1,12 @@
+# path to emr logs in S3
+s3_log_path = ""
+# path to emr_setup.sh in S3
+emr_setup_sh_path = "" 
+# key pair name
+ec2KeyName = ""
+# EC2 subnet id
+ec2SubnetId = ""
+# EMR security group for master node
+emrManagedMasterSecurityGroup = ""
+# EMR security group for slave nodes
+emrManagedSlaveSecurityGroup = ""
