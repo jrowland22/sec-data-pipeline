83acdbad5b2514715413873987b2d027780874ba
jrowland22 <42189132+jrowland22@users.noreply.github.com>
2020-09-01 16:23:35 -0400

    Update emr_setup.sh

--- hdfs/emr_setup.sh
+++ hdfs/emr_setup.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
+source emr.config
+
 wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.48.tar.gz. 
 
 tar -xzf mysql-connector-java-5.1.48.tar.gz
 
 sudo cp mysql-connector-java-5.1.48/mysql-connector-java-5.1.48-bin.jar /usr/lib/sqoop/
 
-sqoop import --connect jdbc:mysql://<path_to_database> --username admin --password password --table Filing --target-dir /user/hadoop/Filing -m 1
+sqoop import --connect jdbc:mysql://$rds_endpoint/$rds_dbname --username $rds_user --password $rds_pass --table Filing --target-dir /user/hadoop/Filing -m 1
 
-sqoop import --connect jdbc:mysql://<path_to_database> --username admin --password password --table Company --columns 'cik,name' --target-dir /user/hadoop/Company -m 1
+sqoop import --connect jdbc:mysql://$rds_endpoint/$rds_dbname --username $rds_user --password $rds_pass --table Company --columns 'cik,name' --target-dir /user/hadoop/Company -m 1
 
 hadoop dfs -ls /user/hadoop/Filing
 
